65f0563bdc6193aabffd91e256c5da7a73acd467
xooooooooox <xozoz.sos@gmail.com>
2025-02-17 10:36:19 +0800

    feat: init

---
+++ volumes/mysql/docker-entrypoint-initdb.d/develop.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS `scaffold-std-demo`;
+-- 拼团交易平台
+CREATE DATABASE IF NOT EXISTS group_buy_market;
+-- 大营销平台系统
+CREATE DATABASE IF NOT EXISTS big_market;
+-- 小型支付商城
+CREATE DATABASE IF NOT EXISTS `s-pay-mall`;
+-- wordpress
+CREATE DATABASE IF NOT EXISTS wordpress;
